9c8d905e6587b93d3a6f53bfd3e5e18fadc1056f
jasonsword <xueyingsong@xueyingutekimbp.airdream>
2013-10-26 10:44:36 +0800

    Add New Project
    add project and third part library

---
+++ EINumbers/EINumbers/Class/AppDelegate/EIAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  EIAppDelegate.h
+//  EINumbers
+//
+//  Created by 薛 迎松 on 13-10-26.
+//  Copyright (c) 2013年 薛 迎松. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EIAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (retain, nonatomic) UIWindow *window;
+
+@end
---
+++ EINumbers/EINumbers/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  EINumbers
+//
+//  Created by 薛 迎松 on 13-10-26.
+//  Copyright (c) 2013年 薛 迎松. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "EIAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([EIAppDelegate class]));
+    }
+}
